dd080da4c2bad7a4ccfd796b310c4e6a47232460
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2025-02-17 09:46:51 +0100

    first commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    #id=models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100,unique=True)
+    language = models.CharField(max_length=100)
+    duration=models.DurationField()
+    description = models.TextField(null=True,blank=True)
+    Coefficient=models.FloatField()
+    startDate=models.DateField(auto_now_add=True)
+    photo=models.ImageField(upload_to='course_photos',blank=True)
+
+
